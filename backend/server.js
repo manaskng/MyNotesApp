de81bc8f230526dcd7a92b948c8a2335a1c8c030
Manasraj2004 <160581417+Manasraj2004@users.noreply.github.com>
2025-07-05 17:41:03 +0530

    correcting server

--- backend/server.js
+++ backend/server.js
@@ -24,10 +24,12 @@ const __dirname = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/frontend/dist")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  
+  app.get("/*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
   });
 }
+
 
 connect();
 
