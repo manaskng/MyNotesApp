39550c9a3c893b6396fc885a953145afa1ec14e6
Manasraj2004 <160581417+Manasraj2004@users.noreply.github.com>
2025-07-05 12:17:18 +0530

    deployment error resolved

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ const __dirname = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/frontend/dist")));
-  app.get("/{*splat}", (req, res) => {
+  app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
   });
 }
